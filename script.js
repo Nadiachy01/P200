613e5f9edfebd1f602472a0ea718633ea08484f2
Nadiachy01 <85254008+Nadiachy01@users.noreply.github.com>
2021-08-16 21:34:40 +0600

    Completed project

--- script.js
+++ script.js
@@ -1,105 +1,276 @@
 const targetDiv = document.getElementById("fp");
-const btn = document.getElementById("button");
+var massage = document.getElementById("msg");
+
+// Difficulties elements
+const easy = document.getElementById("easy");
+const easy2 = document.getElementById("easy2");
+const normal = document.getElementById("normal");
+const normal2 = document.getElementById("normal2");
+const hard = document.getElementById("hard");
+const hard2 = document.getElementById("hard2");
+const wordleft = document.getElementById("wLeft");
+
 var words = document.querySelector(".words");
-var c = document.getElementById("bgMusic"); 
-function playAudio() { 
-  c.play(); 
-} 
-function pauseAudio() { 
-  c.pause(); 
-} 
+var result = document.getElementById("res");
+var scoreShower = document.querySelector(".playerScore");
+
+var temp = document.querySelector(".timer");
+var timerDiv = document.querySelector(".time");
+const gameOver = document.getElementById("Lp");
+
+// Audios variables start
+var carStartSound = document.getElementById("crStrt");
+var carRidingSound = document.getElementById("garir");
+var backgrounMusic = document.getElementById("bgMusic");
+var wrongMusic = document.getElementById("wrong");
+var correctMusic = document.getElementById("right");
+var speedIncreasingSound = document.getElementById("speed");
+var brakeSound = document.getElementById("brake");
+var wonSound = document.getElementById("won");
+var lostSound = document.getElementById("lost");
+var keyPessingSound = document.getElementById("kPres");
+var finishingTimer = document.getElementById("L10s");
+
+var seconds = 0;
+var scoreKeeper = 0;
+var wordLengthKeeper = 0;
+var wordsLeft;
 var spans;
 var typed;
+var musicCheck=1;
+var counter = 0;
 var passedCar = 0;
-var currentWord = '';
+var currentWord = "";
 var checkingIndex = 0;
-var carPaddings = ['2%','3%','4%','11%','12%','16%','17%','21%','22%','26%','27%','31%',
-'32%','36%','37%','41%','42%','46%','47%','51%','52%','56%','57%','61%','62%','66%','67%','71%','72%','73%','74%','75%'];
-var wheelPadding = ['8%', '9%', '10%','17%','18%','22%','23%','27%','28%','32%','33%','37%','38%','42%','43%','47%','48%','52%','53%','57%','58%','62%','63%',
-'67%','68%','72%','73%','77%','78%','79%','80%','81%'];
-btn.onclick = function () {
+var difficultyChek = 0;
+var carPaddings = ["2%","5%","11%","13%","16%","18%","21%","23%","26%","28%","31%","33%","36%","38%","41%","43%","46%","49%","51%","53%","56%","58%","61%","63%","66%","67%","69%","71%","73%","74%","75%"];
+//var wheelPadding = ["8%","11%","17%","19%","22%","24%","27%","29%","32%","34%","37%","39%","42%","44%","47%","49%","52%","54%","57%","59%","62%","64%","67%","69%","71%","72%","75%","77%","79%","80%","81%"];
+
+easy.onclick = function () {
   if (targetDiv.style.display !== "none") {
     targetDiv.style.display = "none";
-  } 
-  else {
-    targetDiv.style.display = "block";
   }
-  var a = document.getElementById("crStrt"); 
-  a.play();
-  var b = document.getElementById("garir"); 
-  b.play();
-  c.play();
-  showWord();
+  difficultyChek = 1;
+  seconds = 120;
+  playGame();
 };
-function typing(e) {
-  typed = String.fromCharCode(e.which);
-  if (spans[checkingIndex].innerHTML === typed) {
-    spans[checkingIndex].classList.add("bg");
-    checkingIndex++;
-  } else {
-    passedCar -= 1;
-    var d = document.getElementById("wrong"); 
-    d.play();
-    var e = document.getElementById("brake"); 
-  e.play();
-    if (passedCar < 0) {
-      passedCar = 0;
-    }
-    moveCar(passedCar);
+
+normal.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
   }
-  if (checkingIndex === spans.length) {
-    document.removeEventListener("keydown", typing, false);
-    passedCar += 1;
-    var f = document.getElementById("right"); 
-    f.play();
-    var g = document.getElementById("speed"); 
-  g.play();
-    if(passedCar == carPaddings.length){
-      passedCar -= 1;
+  difficultyChek = 2;
+  seconds = 90;
+  playGame();
+};
+
+hard.onclick = function () {
+  if (targetDiv.style.display !== "none") {
+    targetDiv.style.display = "none";
+  }
+  difficultyChek = 3;
+  seconds = 60;
+  playGame();
+};
+
+function playGame() {
+  backgrounMusic.currentTime = 0;
+  carStartSound.play(); //music playing
+  carRidingSound.play();
+  if(musicCheck==1){
+  backgrounMusic.play();
+  }
+  showWord(); //Showing Words for typing
+
+  var timer = setInterval(function () {
+    seconds--; //Timer count
+    if (seconds == 10) {
+      finishingTimer.play(); // Last 10 seconds alert Timer
     }
-    checkingIndex = 0;
-    moveCar(passedCar);
-    setTimeout(function () {
-      words.className = "words"; // restart the classes
-      showWord(); // give another word
-      document.addEventListener("keydown", typing, false);
-    }, 400);
+
+    temp.innerHTML = "Time Left : " + seconds; //timer box showing
+
+    if (seconds === 0 || passedCar === 30) {
+      //game ending condition
+      finishingTimer.pause();
+      finishingTimer.currentTime = 0;
+      backgrounMusic.pause();
+      backgrounMusic.currentTime = 0;
+      brakeSound.play();
+      carRidingSound.pause();
+      carRidingSound.currentTime = 0;
+      massage.innerHTML =
+        "Your Score is: " + scoreKeeper + ", Word Typed: " + counter; //Score and typed word count showing
+      if (counter >= 30) {                          //Win or lost massage
+        result.style.color = "lime";
+        result.innerHTML = "You Won!!!";
+        wonSound.play();
+      } 
+      else {
+        lostSound.play();
+        result.style.color = "red";
+        result.innerHTML = "You Lost!!!";
+      }
+      counter = 0;
+      scoreKeeper = 0; //setting all variables to their default values after game ends
+      wordLengthKeeper = 0;
+      wordleft.innerHTML = "Word Left- 30";
+      checkingIndex = 0;
+      scoreShower.innerHTML = "Score : " + scoreKeeper;
+      genText();
+      clearInterval(timer);
+      timerDiv.innerHTML = "sec";
+      passedCar = 0;
+      gameOver.style.display = "block"; //Showing Game ending page
+    }
+  }, 1000);
+}
+
+easy2.onclick = function () {
+  // difficulty selection to play again after the game ends
+  if (gameOver.style.display != "none") {
+    gameOver.style.display = "none";
+  }
+  wonSound.pause();
+  wonSound.currentTime = 0;
+  lostSound.pause();
+  lostSound.currentTime = 0;
+  difficultyChek = 1;
+  seconds = 120;
+  playGame(); //restart the game
+};
+
+normal2.onclick = function () {
+  if (gameOver.style.display != "none") {
+    gameOver.style.display = "none";
+  }
+  wonSound.pause();
+  wonSound.currentTime = 0;
+  lostSound.pause();
+  lostSound.currentTime = 0;
+  difficultyChek = 2;
+  seconds = 90;
+  playGame();
+};
+
+hard2.onclick = function () {
+  if (gameOver.style.display != "none") {
+    gameOver.style.display = "none";
+  }
+  wonSound.pause();
+  wonSound.currentTime = 0;
+  lostSound.pause();
+  lostSound.currentTime = 0;
+  difficultyChek = 3;
+  seconds = 60;
+  playGame();
+};
+
+function typing(e) {
+  // matching typed wort with displayed word
+  typed = String.fromCharCode(e.which);
+  if (typed >= "A" && typed <= "Z") {
+    if (spans[checkingIndex].innerHTML === typed) {
+      spans[checkingIndex].classList.add("bg"); //green marking correct typed letter
+      checkingIndex++;
+      keyPessingSound.play();
+    }
+    else {
+      if (passedCar > 0) {
+        scoreKeeper -= 1;
+        wrongMusic.play();
+        brakeSound.play();
+        passedCar -= 1;
+        wordsLeft = 30 - passedCar;
+        wordleft.innerHTML = "Word Left- " + wordsLeft;
+        scoreShower.innerHTML = "Score : " + scoreKeeper;
+        if (seconds > 10 && difficultyChek == 2) {
+          seconds -= 2;
+        }
+        if (seconds > 10 && difficultyChek == 3) {
+          seconds -= 3;
+        }
+      }
+      if (passedCar <= 0) {
+        passedCar = 0;
+      }
+      moveCar(passedCar); //move the car according typing
+    }
+    if (checkingIndex === spans.length) {
+      //acivity after type all letter correct
+      document.removeEventListener("keydown", typing, false);
+      passedCar += 1;
+      counter += 1;
+      scoreKeeper += wordLengthKeeper;
+      wordsLeft = 30 - passedCar;
+      wordleft.innerHTML = "Word Left- " + wordsLeft;
+      scoreShower.innerHTML = "Score : " + scoreKeeper;
+      correctMusic.play();
+      speedIncreasingSound.play();
+
+      if (passedCar == carPaddings.length) {
+        passedCar -= 1;
+      }
+      checkingIndex = 0;
+      moveCar(passedCar);
+      setTimeout(function () {
+        words.className = "words"; // restart the classes
+        showWord(); // give another word
+        document.addEventListener("keydown", typing, false);
+      }, 400);
+    } 
+    
   }
 }
 function moveCar(passedCar) {
+  //Mopving car to frontward or backward
+  var myCar = document.getElementsByClassName("car");
+  setInterval(cAnim, 40);
+ // var myWheel = document.getElementsByClassName("wheel");
+  //setInterval(wAnim, 40);
+}
 
-  var myCar = document.getElementsByClassName('car')[0];
- setInterval(cAnim,40);
-  var myWheel = document.getElementsByClassName('wheel')[0];
-  setInterval(wAnim,40);
+function cAnim() {
+  document.getElementById("myCar").style.left = carPaddings[passedCar];
+  document.getElementById("myCar").style.transition = "all 2s"; //transition for smooth move
+}
 
-}
-function cAnim(){
-  document.getElementById("myCar").style.left=carPaddings[passedCar];
- // document.getElementById("myCar").style.transition = "all 2s";
-}
-function wAnim(){
- document.getElementById("caka").style.left = wheelPadding[passedCar];
- document.getElementById("caka").style.transition = "all 2s";
-}
+//function wAnim() {
+  //document.getElementById("caka").style.left = wheelPadding[passedCar];
+  //document.getElementById("caka").style.transition = "all 2s";
+//}
 
 document.addEventListener("keydown", typing, false);
+
 function showWord() {
   words.innerHTML = "";
-  var wordArray = genText().split("");
-  for (var i = 0; i < wordArray.length; i++) { //building the words with spans around the letters
-    var span = document.createElement("span");
+  var wordArray = genText().split(""); //score+=wordArray.length;
+  for (var i = 0; i < wordArray.length; i++) {
+    var span = document.createElement("span"); //building the words with spans around the letters
     span.classList.add("span");
     span.innerHTML = wordArray[i];
     words.appendChild(span);
   }
   spans = document.querySelectorAll(".span");
 }
+
 function genText() {
+  //function for generate a word
   var x = Math.floor(Math.random() * word.length);
   var sWord = word[x];
+  wordLengthKeeper = sWord.length;
   return sWord;
 }
+
+function playAudio() { 
+  musicCheck=2;
+  backgrounMusic.play();
+}
+function pauseAudio() {
+  musicCheck=0;
+  backgrounMusic.pause();
+}
+
 const word = ['ACCOUNT', 'ACCURATE', 'ACRES', 'ACROSS', 'ACT', 'ACTION', 'ACTIVE', 'ACTIVITY',
   'ACTUAL', 'ACTUALLY', 'ADD', 'ADDITION', 'ADDITIONAL', 'ADJECTIVE', 'ADULT', 'ADVENTURE',
   'ADVICE', 'AFFECT', 'AFRAID', 'AFTER', 'AFTERNOON', 'AGAIN', 'AGAINST', 'AGE',
@@ -343,5 +514,4 @@ const word = ['ACCOUNT', 'ACCURATE', 'ACRES', 'ACROSS', 'ACT', 'ACTION', 'ACTIVE
   'WRAPPED', 'WRITE', 'WRITER', 'WRITING', 'WRITTEN', 'WRONG', 'WROTE', 'YARD',
   'YEAR', 'YELLOW', 'YES', 'YESTERDAY', 'YET', 'YOU', 'YOUNG', 'YOUNGER',
   'YOUR', 'YOURSELF', 'YOUTH', 'ZERO', 'ZOO'];
-
 
